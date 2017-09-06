9c89c8cef805f0c98a588c6cbe8ecc5983a2b171
Vladimir Polietaiev <vladimir.polietaiev@gmail.com>
2017-09-06 18:48:06 +0300

    add  runApp but don`t work

--- Angular_JS/app.js
+++ Angular_JS/app.js
@@ -2,22 +2,32 @@
     "use strict";
 
     angular.module("todoApp",[])
+        .run(runApp)
         .value("model", {
             user: "Vitaliy",
-            userPhoto: "images/VZ.jpg",
-            items: [
-                { "action": "Estimate...", "done": false },
-                { "action": "Create...", "done": false },
-                { "action": "Edit...", "done": true },
-                { "action": "Delete...", "done": false }
-            ]
+            userPhoto: "images/VZ.jpg"
+            // ,
+            // items: [
+            //     { "action": "Estimate...", "done": false },
+            //     { "action": "Create...", "done": false },
+            //     { "action": "Edit...", "done": true },
+            //     { "action": "Delete...", "done": false }
+            // ]
         })
         .controller("Todo", Todo)
         .filter("checkedItems",checkedItems);
 
+    function runApp($http, model) {
+        $http
+            .get("todo.json")
+            .success(function(data) {
+                model.items = data;
+            });
+    }
+
     function Todo($scope, model) {
         $scope.todo = model;
-        // $scope.showComplete = true;
+        $scope.showComplete = true;
         $scope.addNewItem =addNewItem;
         $scope.incompleteCount = incompleteCount;
         $scope.warningLevel = warningLevel;
