e85ee69c0cf21b4c5f6ae18888af1f1ac2a8a37a
Vladimir Polietaiev <vladimir.polietaiev@gmail.com>
2017-09-08 14:02:50 +0300

    create dayCtrl.js and add ng-controller dayCtrl

---
+++ Angular_JS/Angular_JS_Modul/app/dayCtrl.js
@@ -0,0 +1,13 @@
+(function () {
+    "use strict";
+
+    angular.module("app")
+        .controller("dayCtrl", dayCtrl);
+
+    function dayCtrl($scope) {
+        var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        $scope.day = dayNames[new Date().getDay()];
+    }
+
+})();
