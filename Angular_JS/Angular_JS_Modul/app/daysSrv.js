d139f0fff4e05d43a01ae0ecb510dc2b13fee354
Vladimir Polietaiev <vladimir.polietaiev@gmail.com>
2017-09-08 19:03:57 +0300

    create nowValueSrv.js and add service nowValue

--- Angular_JS/Angular_JS_Modul/app/daysSrv.js
+++ Angular_JS/Angular_JS_Modul/app/daysSrv.js
@@ -1,11 +1,13 @@
 (function () {
     "use strict";
 
-    angular.module("app")
+    angular
+        .module("app")
         .service("days", days);
 
-    function days() {
-        this.today = new Date().getDay();
+    function days(nowValue) {
+        // this.today = new Date().getDay();
+        this.today = nowValue.getDay();
         this.tomorrow = (this.today+1)%7;
     }
 
---
+++ Angular_JS/Angular_JS_Modul/app/nowValueSrv.js
@@ -0,0 +1,9 @@
+(function () {
+    "use strict";
+
+    angular.module("app")
+        .value("nowValue", new Date());
+
+ 
+
+})();
