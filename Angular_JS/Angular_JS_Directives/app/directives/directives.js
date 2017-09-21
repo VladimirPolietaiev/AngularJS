ddcbedb4aedbb5e6f2f066a59a5a3912d0c7e1d0
Vladimir Polietaiev <vladimir.polietaiev@gmail.com>
2017-09-21 19:24:28 +0300

    add directives unorderedList4

--- Angular_JS/Angular_JS_Directives/app/directives/directives.js
+++ Angular_JS/Angular_JS_Directives/app/directives/directives.js
@@ -6,6 +6,7 @@
         .directive("unorderedList1", unorderedList1)
         .directive("unorderedList2", unorderedList2)
         .directive("unorderedList3", unorderedList3)
+        .directive("unorderedList4", unorderedList4)
          ;
     
     
@@ -49,5 +50,27 @@
             }
         };
     }
+
+    function unorderedList4() {
+        return {
+            link:function (scope, element, attrs) {
+                var data = scope[attrs["unorderedList4"]];
+                var propertyName = attrs["listProperty"];
+
+                if(angular.isArray(data)){
+                    var listElem = angular.element("<ul>");
+
+                    angular.forEach(data, function (dataItem) {
+                        // console.log("Item:" + dataItem[propertyName]);
+                        // console.log("Item:" +  scope.$eval(propertyName,dataItem));
+                        listElem.append(angular.element("<li>")
+                            .text( scope.$eval(propertyName,dataItem)));
+                    });
+                    element.append(listElem);
+                }
+            }
+        };
+    }
+
 
 })();
