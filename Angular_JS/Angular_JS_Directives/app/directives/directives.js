bb9f4007e6f3b31a59b20e957983fcd1d1d5e168
Vladimir Polietaiev <vladimir.polietaiev@gmail.com>
2017-09-21 20:58:56 +0300

    add directives scope$watch

--- Angular_JS/Angular_JS_Directives/app/directives/directives.js
+++ Angular_JS/Angular_JS_Directives/app/directives/directives.js
@@ -7,6 +7,7 @@
         .directive("unorderedList2", unorderedList2)
         .directive("unorderedList3", unorderedList3)
         .directive("unorderedList4", unorderedList4)
+        .directive("unorderedList5", unorderedList5)
          ;
     
     
@@ -65,6 +66,36 @@
                         // console.log("Item:" +  scope.$eval(propertyName,dataItem));
                         listElem.append(angular.element("<li>")
                             .text( scope.$eval(propertyName,dataItem)));
+                    });
+                    element.append(listElem);
+                }
+            }
+        };
+    }
+
+    function unorderedList5() {
+        return {
+            link:function (scope, element, attrs) {
+                var data = scope[attrs["unorderedList5"]];
+                var propertyName = attrs["listProperty"];
+
+                if(angular.isArray(data)){
+                    var listElem = angular.element("<ul>");
+
+
+                    angular.forEach(data, function (dataItem) {
+                        var itemElem = angular.element("<li>");
+                        listElem.append(itemElem);
+
+                        var watchFn = function (watchScope) {
+                            return watchScope.$eval(propertyName,dataItem);
+                        };
+
+                        scope.$watch(watchFn, function (newVal, oldVal) {
+                            itemElem.text(newVal);
+                        });
+
+
                     });
                     element.append(listElem);
                 }
